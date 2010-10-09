bdc9e61dcab8ffec1c12675cbbbbb2512c69fd35
Philip Damra <philip@philipdamra.com>
2010-10-08 21:12:29 -0400

    truncated long titles and descriptions on homepage blog list

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -12,18 +12,7 @@ class IndexController extends Zend_Controller_Action
     {
     	$this->_helper->layout->setLayout('home');
         $blogModel = new Model_Blog();
-        $recentBlogs = $blogModel->getRecentBlogs('4');
-        $featuredItems = array();
-        if(is_array($recentBlogs))
-        {
-        	for($i=1; $i<=3; $i++)
-        	{
-        		if(count($recentBlogs) > 0)
-        		{
-        			$featuredItems[] = array_shift($recentBlogs);
-        		}
-        	}
-        }
+        $featuredItems = $blogModel->getRecentBlogs('3');
         $this->view->featuredItems = $featuredItems;
     }
 }
